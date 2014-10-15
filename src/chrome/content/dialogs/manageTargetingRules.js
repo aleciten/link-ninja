6571171aa1c3e0d10fbf735d6e637f9a20894c08
aleciten <abecher@gmail.com>
2014-10-15 19:56:10 -0300

    Added copyright notices

--- src/chrome/content/dialogs/manageTargetingRules.js
+++ src/chrome/content/dialogs/manageTargetingRules.js
@@ -1,3 +1,16 @@
+// This file is part of Link Ninja (http://github.com/aleciten/link-ninja)
+//
+// Copyright (C) 2014 Alejandro Becher
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
 var _                 = window.opener.aleciten.linkNinja._,
     tree              = null,    
     filterRadioGroup  = null,
--- src/chrome/content/dialogs/options.js
+++ src/chrome/content/dialogs/options.js
@@ -1,3 +1,16 @@
+// This file is part of Link Ninja (http://github.com/aleciten/link-ninja)
+//
+// Copyright (C) 2014 Alejandro Becher
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
 var windowsService = Components.classes['@mozilla.org/appshell/window-mediator;1'].getService(Components.interfaces.nsIWindowMediator),
     browserWindow  = windowsService.getMostRecentWindow('navigator:browser'),
     prefService    = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService),    
